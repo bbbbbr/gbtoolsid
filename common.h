2e8d84514a4f85cb8858b463ff3f95086104fd70
bbbbbr <bbbbbr@users.noreply.github.com>
2021-05-12 00:09:57 -0700

    Remove spaces between commas and quotes in csv, add bare csv style, improve field names

--- common.h
+++ common.h
@@ -5,8 +5,9 @@
 #define MAX_FILE_STR 4096
 #define MAX_STR_LEN  2048
 
-#define OUTPUT_DEFAULT 0
-#define OUTPUT_JSON    1
-#define OUTPUT_CSV     2
+#define OUTPUT_DEFAULT  0
+#define OUTPUT_JSON     1
+#define OUTPUT_CSV      2
+#define OUTPUT_CSV_BARE 3
 
 #endif // _COMMON_H
