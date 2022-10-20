ed691f13c12479aced2e071a87ee541fb74d7dc7
bbbbbr <bbbbbr@users.noreply.github.com>
2022-10-19 22:55:28 -0700

    Tools: GBStudio 3.x.x detection, GBDK with crashhandler

--- src/sig_gbstudio.c
+++ src/sig_gbstudio.c
@@ -71,10 +71,23 @@ bool check_gbstudio(void) {
         }
     }
 
-    // GBStudio 3.0.0 alpha +
+    // GBStudio 3.0.0+
     if (find_pattern(sig_gbs_musicmanager_c_FX_ADDR_LO_3_0_0_alpha1_plus, sizeof(sig_gbs_musicmanager_c_FX_ADDR_LO_3_0_0_alpha1_plus)) &&
         find_pattern(sig_gbs_math_c_sinetable_3_0_0_alpha1_plus, sizeof(sig_gbs_math_c_sinetable_3_0_0_alpha1_plus))) {
-        entry_add_with_version(entry, "3.0.0 alpha 1+");
+        // entry_check_match() Relies on GBDK tool check being run before ZGB is tested (it is)
+
+        // GBStudio 3.0.0 alpha 1+ uses GBDK 4.0.4
+        if (entry_check_match(TYPE_TOOLS, STR_GBDK, STR_GBDK_2020_4_0_4)) {
+            entry_add_with_version(entry, "3.0.0 alpha 1+");
+            return true;
+        }
+        // GBStudio 3.0.0 official - 3.1.0+ uses GBDK 4.0.5 & 4.0.6 (which cannot be differentiated
+        else if (entry_check_match(TYPE_TOOLS, STR_GBDK, STR_GBDK_2020_4_0_5_to_4_0_6)) {
+            entry_add_with_version(entry, "3.0.0 - 3.1.0+");
+            return true;
+        }
+
+        entry_add_with_version(entry, "3.x.x Unknown");
         return true;
     }
 
