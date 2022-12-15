b17b083ae78f3b39a82fab9265107e1d7e42f119
bbbbbr <bbbbbr@users.noreply.github.com>
2022-12-14 22:47:08 -0800

    Tools: DevSound X, more macros to copy & paste C into JS (not sure it's worth it)

--- src/common.h
+++ src/common.h
@@ -26,4 +26,12 @@
 #define CHECK_PATTERN_AT_ADDR(ptr_to_buf, check_addr) (check_pattern_addr(ptr_to_buf, sizeof(ptr_to_buf), check_addr))
 #define FORMAT_ENTRY(e_type, e_name, e_version) ((tool_entry){.type = e_type, .c_name = e_name, .c_version = e_version})
 
+// Reference example: const uint8_t sig_str_devsound_lite[] = "DevSound Lite";
+#define DEF_PATTERN_STR(varname, str_content) const uint8_t varname[] = str_content
+
+// Reference example: const uint8_t sig_gbsoundsystem_MultiSFXLoop[] = {0x2A, 0x4E, 0x06, 0x00, 0x87, 0xCB, 0x10, 0x87, 0xCB, 0x10, 0x87, 0xCB, 0x10};
+// Need an additional macro wrapper AR_ARGS() to pass and expand a set of values to the array declaration
+#define AR_ARGS(...) __VA_ARGS__
+#define DEF_PATTERN_BUF(varname, buf_content) const uint8_t varname[] = {buf_content}
+
 #endif // _COMMON_H
